0125d980079d2a605a901199a9db85f848485703
Mat Sz <contact@matsz.dev>
2020-05-31 14:16:36 +0200

    added baseweb

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { Client as Styletron } from 'styletron-engine-atomic';
+import { Provider as StyletronProvider } from 'styletron-react';
+import { LightTheme, BaseProvider, styled } from 'baseui';
 
 import App from './App';
 import createStore from './store';
 
 const store = createStore();
+const engine = new Styletron();
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <StyletronProvider value={engine}>
+        <BaseProvider theme={LightTheme}>
+          <App />
+        </BaseProvider>
+      </StyletronProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
