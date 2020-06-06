5830a3c8ef7069b33798805721cb1f2293ed886e
Mat Sz <contact@matsz.dev>
2020-06-06 17:03:08 +0200

    added current user information to header

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,14 +1,36 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Unstable_AppNavBar as AppNavBar } from 'baseui/app-nav-bar';
+import { Overflow as UserIcon } from 'baseui/icon';
+
+import { StateType } from '../reducers';
+
+function renderItem(item: any) {
+  return item.label;
+}
+
+const USER_NAV = [
+  {
+    icon: UserIcon,
+    item: { label: 'Settings' },
+    mapItemToNode: renderItem,
+    mapItemToString: renderItem,
+  },
+];
 
 const NavBar: React.FC = () => {
   const noop = () => false;
+  const currentUser = useSelector((state: StateType) => state.currentUser);
 
   return (
     <AppNavBar
       appDisplayName={'Kreds'}
       isNavItemActive={noop}
       onNavItemSelect={noop}
+      username={currentUser?.name}
+      usernameSubtitle={currentUser?.fullName}
+      userImgUrl=""
+      userNav={currentUser ? USER_NAV : undefined}
     />
   );
 };
